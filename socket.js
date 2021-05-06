fd196eed76d409aebc0d92e521ea28c1aacb5d9a
Trọng Hiếu <tronghieu60s@gmail.com>
2021-05-06 20:55:52 +0700

    socket send answer

--- socket.js
+++ socket.js
@@ -1,9 +1,14 @@
 const socketConnect = (io) => {
   const users = {};
+  const quiz = {};
   io.on("connection", (socket) => {
     // socket admin user
-    socket.on("client-join-control", (quiz_code) => {
+    socket.on("client-join-control", (props) => {
+      const { quiz_code } = props;
+      // setup quiz
       if (!users[quiz_code]) users[quiz_code] = [];
+      if (!quiz[quiz_code]) quiz[quiz_code] = {};
+
       socket.join(quiz_code + "-control");
       socket.emit("server-send-users", users[quiz_code]);
     });
@@ -22,6 +27,16 @@ const socketConnect = (io) => {
     socket.on("client-send-quiz", (props) => {
       const { quiz_code, question } = props;
       io.to(quiz_code).emit("server-send-quiz", question);
+    });
+
+    socket.on("client-show-answer", (props) => {
+      const { quiz_code } = props;
+      quiz[quiz_code]["show-answer"] = true;
+      socket.emit("server-show-answer", quiz[quiz_code]["show-answer"]);
+      io.to(quiz_code).emit(
+        "server-show-answer",
+        quiz[quiz_code]["show-answer"]
+      );
     });
 
     // socket normal user
