c848e847881ed2d6b4bc29a9b14440bb6aacd896
Trọng Hiếu <tronghieu60s@gmail.com>
2021-05-09 11:16:51 +0700

    optimize server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 require("dotenv").config({ path: ".env.local" });
-const createError = require("http-errors");
 const express = require("express");
 const logger = require("morgan");
 
@@ -8,26 +7,8 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(logger("dev"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// Catch 404
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-// eslint-disable-next-line no-unused-vars
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send({ error: err.message });
-});
 
 const socketConfig = {
   cors: {
