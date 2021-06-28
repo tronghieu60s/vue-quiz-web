2cad6abfa8e700ba610df31727948279673e62eb
Trọng Hiếu <tronghieu60s@gmail.com>
2021-06-28 11:02:06 +0700

    mongoose database

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
-const express = require("express");
 const logger = require("morgan");
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
+/* Middleware */
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/* MongoDB */
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/myproject", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("✔️ Connected To Database Successfully!"))
+  .catch((err) => console.log(`❌ Failed To Connect To Database!\n ${err}`));
+
+/* SocketIO */
 const socketConfig = {
   cors: {
     origin: "*",
@@ -20,6 +33,7 @@ const io = require("socket.io")(server, socketConfig);
 
 require("./socket")(io);
 
+/* Listen Server */
 server.listen(PORT, () =>
   console.log(`Server is running at http://localhost:${PORT}/`)
 );
