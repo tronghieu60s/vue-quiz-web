386b6f428a9e772c9e81ac23269272869487d0da
Trọng Hiếu <tronghieu60s@gmail.com>
2021-06-29 20:41:15 +0700

    setup cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 require("module-alias/register");
+const cors = require("cors");
 const logger = require("morgan");
 const express = require("express");
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 /* Middleware */
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
